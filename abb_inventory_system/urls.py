c0b3e4198d321affe032f721a485e5895024c535
obinnho00 <arumi21@studens.ecu.edu>
2026-02-28 20:19:15 -0800

    add image functionality rework

--- abb_inventory_system/urls.py
+++ abb_inventory_system/urls.py
@@ -1,29 +1,13 @@
 """
-URL configuration for lineWatch project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+URL configuration for abb_inventory_system project.
 """
-from django.contrib import admin
-from django.urls import path
-from inventory import views
-
 
 from django.contrib import admin
 from django.urls import path
 from inventory import views
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,14 +15,15 @@ urlpatterns = [
     path('', views.Home, name='home'),
 
     path('submit-requirement/', views.handle_requirement_submission, name='submit_requirement'),
+
     path('inventory/upload-part-image/', views.upload_part_image_popup, name='upload_part_image_popup'),
+
     path("inventory/search/", views.inventory_search, name="inventory_search"),
 
-    # MAIN INVENTORY PAGE (handles search + department filter)
+    # MAIN INVENTORY PAGE
     path('inventory/', views.inventory_view, name='inventory'),
 ]
 
-if settings.DEBUG and settings.MEDIA_URL and settings.MEDIA_ROOT:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
+# Serve media files (persistent disk setup)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
